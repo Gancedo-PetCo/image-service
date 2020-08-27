a514fd77f0e5949f7fd13ade1ecfd85d605ef518
Nick-Zabalza <nickyz2565@gmail.com>
2020-08-26 22:05:11 -0700

    WIP. Updated seed to handle more generation and new format. Not Tested fully. Still need to update Schema and endpoints

--- database-mongodb/insert_images.js
+++ database-mongodb/insert_images.js
@@ -1,47 +1,77 @@
 const Images = require('./Images.js');
-const url = require('url');
 
-let insertImages = function (urls) {
-  let imageArray = modifyImageData(urls);
-  let allImagesObject = groupImageData(imageArray);
-  return Images.insertRecords(Object.values(allImagesObject));
-}
+// let modifyImageData = function(imageUrls) {
+//   let imageArray = [];
+//   let sizesObj = {small: '54', medium: '400', large: '1000'}
+//   for (let value of imageUrls) {
+//     let imageObject = {};
+//     for (let size in sizesObj) {
+//       let parsed = url.parse(value, true);
+//       parsed.query.w = sizesObj[size];
+//       delete parsed.search;
+//       imageObject[size] = url.format(parsed);
+//     }
+//     imageArray.push(imageObject);
+//   }
+//   return imageArray;
+// }
 
-let modifyImageData = function(imageUrls) {
-  let imageArray = [];
-  let sizesObj = {small: '54', medium: '400', large: '1000'}
-  for (let value of imageUrls) {
-    let imageObject = {};
-    for (let size in sizesObj) {
-      let parsed = url.parse(value, true);
-      parsed.query.w = sizesObj[size];
-      delete parsed.search;
-      imageObject[size] = url.format(parsed);
+//This function now generates a thousand randomly generated records at a time.
+const groupImageData = (urls, batch) => {
+  const batchOfItemObjects = [];
+  const startingItemIdForThisBatch = 1000 * batch + 100;
+  const amountOfObjectItemsToBeRandomlyGenerated = 1000;
+  let count = 0;
+
+  while (count < amountOfObjectItemsToBeRandomlyGenerated) {
+    const catOrDog = Math.floor(Math.random() * 2);
+    let startIndex = 0;
+    let endIndex = urls.length / 2;
+
+    if (catOrDog) {
+      startIndex = endIndex;
+      endIndex = urls.length;
     }
-    imageArray.push(imageObject);
-  }
-  return imageArray;
-}
 
-let groupImageData = (imageArray) => {
-  let allImagesObject = {};
-  let itemId = 100;
-  let imageNum = 1;
-  for (let imageObj of imageArray) {
-    if (itemId <= 199 && imageNum === 1) {
-      allImagesObject[itemId] = {itemId: itemId, itemImages: [imageObj]};
-    } else if (itemId > 199 && imageNum < 4) {
-      itemId = 100;
-      imageNum += 1;
-      allImagesObject[itemId].itemImages.push(imageObj);
-    } else if (itemId <= 199 && (imageNum > 1 && imageNum < 4)) {
-      allImagesObject[itemId].itemImages.push(imageObj);
-    } else {
-      break;
+
+    let numberOfPicturesItemHas = Math.ceil(Math.random() * 3);
+    const picturesItemHas = [];
+
+    while (numberOfPicturesItemHas > 0) {
+      const randomPictureIndex = Math.floor(Math.random() * (endIndex - startIndex)) + startIndex;
+      const potentialPicture = urls[randomPictureIndex];
+
+      if (picturesItemHas.indexOf(potentialPicture) === -1) {
+        picturesItemHas.push(picturesItemHas);
+        numberOfPicturesItemHas--;
+      }
     }
-    itemId += 1;
+
+    const itemId = (startingItemIdForThisBatch + count).toString();
+    const picturesItemHasString = picturesItemHas.join('XXX');
+
+    batchOfItemObjects.push({ itemId, picturesItemHasString });
+
+    count++;
   }
-  return allImagesObject;
-}
+
+  return batchOfItemObjects;
+};
+
+const totalNumberOfBatches = 2;
+//DELETE AND UNCOMMENT
+// const totalNumberOfBatches = 10000;
+
+const insertImages = function (urls) {
+  const dataInsertions = [];
+
+  for (let i = 0; i < totalNumberOfBatches; i++) {
+    const batchOfItemObjects = groupImageData(urls, i);
+
+    dataInsertions.push(Images.insertRecords(batchOfItemObjects));
+  }
+
+  return Promise.all(dataInsertions);
+};
 
 module.exports = insertImages;
