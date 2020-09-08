0508d134fead42bfcaf0dfe72e2cb5170ab89d2a
Nick-Zabalza <nickyz2565@gmail.com>
2020-09-08 16:01:16 -0700

    Committing before merging a different branch

---
+++ database-mongodb/MySQL/testDatabseQueryEfficiency.test.js
@@ -0,0 +1,20 @@
+const {
+  singleItemData,
+  generateSingleItemData,
+  arrayItemData,
+  generateArrayItemData,
+  extractSingleItem,
+  makeActualCall,
+  makeActualCallSync,
+  singleItemResponseData,
+  singleItemResponseQueryTimes,
+  testMySQLImagesSingle,
+  testMySQLImagesSingleSync,
+  multiItemResponseData,
+  multiItemResponseQueryTimes,
+  testMySQLImagesMulti,
+  testMySQLImagesMultiSync,
+  analyzeResults,
+  handleTests
+} = require('./testDatabaseQueryEfficiency.js');
+
